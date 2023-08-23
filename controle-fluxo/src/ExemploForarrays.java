ac23f58836fa662bd132cb4c7b94d9a8699ca528
samuellparra <samfreesurfer@hotmail.com>
2023-08-23 12:03:54 -0300

    Exemplos

---
+++ controle-fluxo/src/ExemploForarrays.java
@@ -0,0 +1,10 @@
+public class ExemploForarrays {
+    public static void main(String[] args) {
+        String alunos [] = {"Felipe", "Junior", "Henrique", "Vitor"};
+
+            for(String aluno : alunos){
+                System.out.println("nome do aluno é:" + aluno);
+                
+            }
+    }
+}
