1b01590cd27d6befeb26fe56abfecfe6c3ecc798
samuellparra <samfreesurfer@hotmail.com>
2023-08-23 10:31:31 -0300

    Controle Fluxo

---
+++ controle-fluxo/src/CaixaEletronico.java
@@ -0,0 +1,17 @@
+public class CaixaEletronico {
+    public static void main(String[] args) {
+        double saldo = 25.0;
+        double valorSolicitado = 17.0;
+
+        if(valorSolicitado < saldo){
+                    saldo = saldo - valorSolicitado;
+            System.out.println("Saldo atual:" + saldo);
+        } 
+        else
+        System.out.println("Saldo insuficiente.");
+        
+    
+        System.out.println(saldo);
+                    
+    }
+}
